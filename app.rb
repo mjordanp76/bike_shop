ff2eea52d6d4c6c584f877f87cc5a6c327221bd6
Jordan <mpjordan76@gmail.com>
2026-05-21 16:53:54 -0400

    added register page & appropriate links/logic

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'pg'
 require 'bcrypt'
 require 'dotenv/load'
 
+enable :sessions
+
 def db_connect
   @db ||= PG.connect(
     dbname: ENV['DATABASE_NAME']
@@ -29,4 +31,25 @@ get '/bikes/:id' do
   @bike = result.first
 
   erb :bike
+end
+
+get '/register' do
+  erb :register
+end
+
+post '/register' do
+
+  password_hash =
+    BCrypt::Password.create(params[:password])
+
+  db.exec_params(
+    "INSERT INTO users (email, password_hash)
+     VALUES ($1, $2)",
+    [
+      params[:email],
+      password_hash
+    ]
+  )
+
+  redirect '/'
 end
