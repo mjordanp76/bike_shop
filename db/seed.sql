e30158820af937ab515879e20e1b73a84f17d516
Jordan <mpjordan76@gmail.com>
2026-05-22 02:10:06 -0400

    added seed.sql for bicycle table population upon deployment

---
+++ db/seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO bicycles (name, description, price, inventory, img_url)
+VALUES
+('Roadrunner', 'A lightweight aluminum frame, ideal for a beginner wanting to go fast.', 1899.99, 10, ''),
+('Falcon', 'A lightweight carbon fiber frame, ideal for serious riders who seek speed and agility.', 2499.99, 10, ''),
+('Warthog', 'A durable steel frame ideal for anyone just getting into the exhilarating sport of gravel riding.', 1699.99, 10, ''),
+('Jackal', 'A spritely titanium frame, great for more experienced riders to shred the logging roads.', 1899.99, 10, ''),
+('Rhino', 'A beginner-friendly mountain bike great for tackling mountain roads.', 1599.99, 10, ''),
+('Beast', 'A serious, full-suspension mountain bike for those looking to fly down bumpy single-track courses.', 2399.99, 10, ''),
+('Egret', 'A lightweight aluminum frame, with handlebar basket, great for cruising around the city running errands.', 699.99, 10, ''),
+('Albatross', 'With a handlebar basket and rear rack, this is ideal for anyone wanting to commute by bike and pick up some groceries on the way home.', 999.99, 10, ''),
+('Zappy', 'A 200-watt electric-assisted bicycle great for those who need just a little extra help getting up the hills.', 1499.99, 10, ''),
+('Bolt', 'A 500-watt electric bicycle great for those aging knees. Take on the hills and long rides again with confidence!', 2599.99, 10, '');
